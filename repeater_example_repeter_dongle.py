f6efa41fafbe343e3813ff72c565be13b9841d6d
Emil Lindblom <emil@smartsensordevices.com>
2021-03-30 13:10:45 +0200

    bugfixes 13:10 30/3-21

--- repeater_example_repeter_dongle.py
+++ repeater_example_repeter_dongle.py
@@ -32,23 +32,8 @@ def send_msg(buffer):
         msg_to_send = result_array[1]
         print("Forwarding data to reciever:")
         print(msg_to_send)
-        time.sleep(0.1)
-        target_conn = repeter_dongle.at_target_conn()
-        target_conn = target_conn[0].split("=")
-        target_conn = target_conn[1]
-        # print(target_conn)
+        repeter_dongle.at_spssend(msg_to_send)
         repeter_dongle.rx_state = "rx_waiting"
-        if not msg_to_send == "":
-            for conn in connection_list:
-                if not conn == dongle_conn_idx:
-                    if not conn == target_conn:
-                        repeter_dongle.at_target_conn(conn)
-                        time.sleep(0.1)
-                        repeter_dongle.rx_state = "rx_waiting"
-                        time.sleep(0.1)
-                    repeter_dongle.at_spssend(msg_to_send)
-                    time.sleep(0.1)
-                    repeter_dongle.rx_state = "rx_waiting"
     except:
         print(".")
 
@@ -57,34 +42,30 @@ try:
     while 1:
         buffer = repeter_dongle.rx_buffer.decode("utf-8", "ignore")
         if "\r\nCONNECTED." in buffer:
-            if "handle_evt_gap_connected: conn_idx=" in buffer:
-                new_conn_inx = buffer
-                new_conn_inx = new_conn_inx.split("\r\n")
-                for line in new_conn_inx:
-                    if "handle_evt_gap_connected: conn_idx=" in line:
-                        c_idx = line.split(" ")
-                        # print(c_idx)
-                        c_idx = c_idx[1].split("=")
-                        c_idx = c_idx[1]
-                        print("CONN_INDEX=" + c_idx)
-                        connection_list.append(c_idx)
-                        print("connection_list=")
-                        print(connection_list)
-                        # print(line)
+            # if "handle_evt_gap_connected: conn_idx=" in buffer:
+            #     new_conn_inx = buffer
+            #     new_conn_inx = new_conn_inx.split("\r\n")
+            #     for line in new_conn_inx:
+            #         if "handle_evt_gap_connected: conn_idx=" in line:
+            #             c_idx = line.split(" ")
+            #             c_idx = c_idx[1].split("=")
+            #             c_idx = c_idx[1]
+            #             connection_list.append(c_idx)
             num_of_connected_devices = num_of_connected_devices + 1
-            print("A DONGLE HAVE CONNECTED!")
+            print("A Dongle has connected!")
             repeter_dongle.at_advstart()
             repeter_dongle.rx_state = "rx_waiting"
         if "\r\nDISCONNECTED." in buffer:
             num_of_connected_devices = num_of_connected_devices - 1
             connection_list = []
-            print("NO CONNECTIONS.")
+            print("No Dongles connected.")
         if "\r\nconn_idx=" in buffer:
             for conn in connection_list:
                 if "\r\nconn_idx=" + conn + " DISCONNECTED.\r\n" in buffer:
                     connection_list.remove(conn)
                     num_of_connected_devices = num_of_connected_devices - 1
-                    print("DONGLE=" + conn + " DISCONNECTED.")
+                    # print("DONGLE=" + conn + " DISCONNECTED.")
+                    print("A Dongle has disconnected!")
         if num_of_connected_devices > 1:
             if "\r\n[Received]:" in buffer:
                 send_msg(buffer)
--- repeater_example_sender_dongle.py
+++ repeater_example_sender_dongle.py
@@ -1,5 +1,6 @@
 import time
 from bleuio_lib.bleuio_funcs import BleuIo
+import random
 
 sender_dongle_port = "COM38"
 mac_addr_to_repeater = "[0]40:48:FD:E5:2D:9C"
@@ -14,7 +15,7 @@ buffer = ""
 print("Dongle found.")
 
 try:
-    print(sender_dongle.at_dual())
+    sender_dongle.at_dual()
     sender_dongle.rx_state = "rx_waiting"
     ready = input(
         "Press enter to connect to the repeater dongle. (This should be connected last)."
@@ -41,34 +42,36 @@ try:
     ready = input("Press enter to start sending data to the repeater dongle.")
 
     while 1:
-        print(sender_dongle.at_findscandata("FF5B07"))
-        time.sleep(3)
-        sender_dongle.stop_scan()
-        time.sleep(0.5)
-        result_list = sender_dongle.rx_scanning_results
-        if not result_list == []:
-            for results in result_list:
-                lines = results.split("\r\n")
-                for line in lines:
-                    if not line == "":
-                        data_array = line.split(" ")
-                        lenght = len(data_array)
-                        if lenght == 5:
-                            data = data_array[4]
-                            print(data)
-                            break
-        else:
-            sender_dongle.stop_scan()
-            sender_dongle.rx_state = "rx_waiting"
-        time.sleep(2)
+        # print(sender_dongle.at_findscandata("FF5B07"))
+        # time.sleep(3)
+        # sender_dongle.stop_scan()
+        # time.sleep(0.5)
+        # result_list = sender_dongle.rx_scanning_results
+        # if not result_list == []:
+        #     for results in result_list:
+        #         lines = results.split("\r\n")
+        #         for line in lines:
+        #             if not line == "":
+        #                 data_array = line.split(" ")
+        #                 lenght = len(data_array)
+        #                 if lenght == 5:
+        #                     data = data_array[4]
+        #                     print(data)
+        #                     break
+        # else:
+        #     sender_dongle.stop_scan()
+        #     sender_dongle.rx_state = "rx_waiting"
+        # time.sleep(2)
+        data = "sender_data" + str(random.randint(0, 1000 - 1))
         if not data == "":
-            print(sender_dongle.at_spssend(data))
+            sent = sender_dongle.at_spssend(data)
             sender_dongle.rx_state = "rx_waiting"
+            if "[Sent]" in sent[0]:
+                print("Data = (" + data + ") sent.")
+            time.sleep(5)
             data = ""
         print("Loop.")
         time.sleep(0.1)
 except KeyboardInterrupt:
-    sender_dongle.rx_state = "rx_waiting"
-    sender_dongle.stop_scan()
     sender_dongle.at_gapdisconnect()
     print("Shutting down script.")
